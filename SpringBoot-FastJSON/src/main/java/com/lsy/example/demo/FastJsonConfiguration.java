92b976236809fddad4f29e62c032a4e238903087
islsy <lsyol@outlook.com>
2018-09-07 15:39:56 +0800

    SpringBoot项目上使用SpringDataJPA完成CRUD

--- SpringBoot-FastJSON/src/main/java/com/lsy/example/demo/FastJsonConfiguration.java
+++ SpringBoot-FastJSON/src/main/java/com/lsy/example/demo/FastJsonConfiguration.java
@@ -3,14 +3,11 @@ package com.lsy.example.demo;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import sun.plugin2.message.Serializer;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
